3cd4ee7c90170b10ab2cfafbd526b916cd477f95
parmaster <tony.racomore@gmail.com>
2024-06-02 00:44:02 +0300

    #IH0 extract URL path and answer with 404

--- app/server.go
+++ app/server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"strconv"
@@ -36,52 +38,71 @@ func (s *Server) ListenAndServe() error {
 	}
 }
 
+// primitive router
+func (s *Server) respond(req Request) Response {
+	resp := NewResponse()
+
+	if req.method == "GET" {
+
+		resp.code = 200
+		resp.reason = "OK"
+		resp.headers = map[string]string{}
+		resp.body = []byte{}
+
+		// hardcoded routing, 404 for rabdom target
+		if req.target != "/" {
+			resp.code = 404
+			resp.reason = "Not Found"
+		}
+
+	}
+
+	return *resp
+}
+
 func (s *Server) handleConnection(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	for {
-		request, err := s.readPacket(reader)
+		request := Request{}
+		err := request.Read(reader)
 		if err != nil {
-			log.Printf("[ERROR] %v", err)
-			continue
+			if errors.Is(err, io.EOF) {
+				log.Printf("[INFO] Connection closed (EOF)")
+				return // connection closed
+			} else {
+				log.Printf("[ERROR] %v", err)
+				continue
+			}
 		}
-
-		if request.method == "GET" {
-			r := NewResponse()
-			code := 200
-			reason := "OK"
-			headers := []string{}
-			body := []byte{}
-			response := r.format(code, reason, headers, body)
-			conn.Write([]byte(response))
-		}
-
 		log.Printf("[DEBUG] request parsed: %+v", request)
+
+		resp := s.respond(request)
+		conn.Write([]byte(resp.format()))
 	}
 }
 
-type Packet struct {
+type Request struct {
 	method  string
-	request string
+	target  string
 	version string
 	headers map[string]string
 	body    []byte
 }
 
-func (s *Server) readPacket(reader *bufio.Reader) (Packet, error) {
-	p := Packet{}
+func (req *Request) Read(reader *bufio.Reader) error {
 	// https://datatracker.ietf.org/doc/html/rfc9112#name-message-format
 	// GET /qwe/rty HTTP/1.1
 	startLine, err := reader.ReadString('\n')
 	if err != nil {
 		err := fmt.Errorf("error reading start-line: %w", err)
-		return p, err
+		return err
 	}
 	startLine = strings.Trim(startLine, "\r\n")
 	startLineParts := strings.Split(startLine, " ")
 
-	p.method = startLineParts[0]
-	p.request = startLineParts[1]
-	p.version = startLineParts[2]
+	req.method = startLineParts[0]
+	req.target = startLineParts[1]
+	req.version = startLineParts[2]
 
 	// headers are optional. example:
 	// Host: localhost:4221
@@ -95,7 +116,7 @@ func (s *Server) readPacket(reader *bufio.Reader) (Packet, error) {
 		}
 		// reached the end of the headers section?
 		if headerLine == "\r\n" {
-			p.headers = headers
+			req.headers = headers
 			break
 		}
 		headerLineParts := strings.SplitN(headerLine, ":", 2)
@@ -105,15 +126,19 @@ func (s *Server) readPacket(reader *bufio.Reader) (Packet, error) {
 		}
 	}
 
-	p.body = []byte{}
+	req.body = []byte{}
 
 	log.Printf("[DEBUG] startLineParts: %v", startLineParts)
 	log.Printf("[DEBUG] headers: %v", headers)
 
-	return p, nil
+	return nil
 }
 
 type Response struct {
+	code    int
+	reason  string
+	headers map[string]string
+	body    []byte
 	version string
 }
 
@@ -124,13 +149,17 @@ func NewResponse() *Response {
 	return &r
 }
 
-func (r *Response) format(code int, reason string, headers []string, body []byte) string {
+func (r *Response) format() string {
 	result := ""
 
-	result = fmt.Sprintf("%s%s %d %s\r\n", result, r.version, code, reason)
-	result = fmt.Sprintf("%s%s\r\n", result, strings.Join(headers, ""))
+	result = fmt.Sprintf("%s%s %d %s\r\n", result, r.version, r.code, r.reason)
+	for k, v := range r.headers {
+		result = fmt.Sprintf("%s%s: %s\r\n", result, k, v)
+	}
+	result = fmt.Sprintf("%s\r\n", result)
 
-	for _, v := range body {
+	// temporary stub
+	for _, v := range r.body {
 		result += string(v)
 	}
 
--- app/server_test.go
+++ app/server_test.go
@@ -11,32 +11,80 @@ import (
 func Test_OKResponse(t *testing.T) {
 
 	r := NewResponse()
-	code := 200
-	reason := "OK"
-	headers := []string{}
-	body := []byte{}
-	assert.Equal(t, "HTTP/1.1 200 OK\r\n\r\n", r.format(code, reason, headers, body))
+	r.code = 200
+	r.reason = "OK"
+	r.headers = map[string]string{}
+	r.body = []byte{}
+	assert.Equal(t, "HTTP/1.1 200 OK\r\n\r\n", r.format())
 }
 
-func Test_ReadPacket(t *testing.T) {
+func Test_ReadRequest(t *testing.T) {
 
 	r := "GET /qwe/rty HTTP/1.1\r\n"
 	r += "Host: localhost:4221\r\n"
 	r += "User-Agent: curl/8.4.0\r\n"
 	r += "Accept: */*\r\n\r\n"
 
-	s := NewServer(0)
+	reader := bufio.NewReader(strings.NewReader(r))
+	request := Request{}
+	err := request.Read(reader)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "GET", request.method)
+	assert.Equal(t, "/qwe/rty", request.target)
+	assert.Equal(t, "HTTP/1.1", request.version)
+	assert.Equal(t, "localhost:4221", request.headers["Host"])
+	assert.Equal(t, "curl/8.4.0", request.headers["User-Agent"])
+	assert.Equal(t, "*/*", request.headers["Accept"])
+	assert.Equal(t, []byte{}, request.body)
+}
+
+func Test_ReadTwoRequests(t *testing.T) {
+
+	r := "GET / HTTP/1.1\r\n"
+	r += "Host: localhost:4221\r\n"
+	r += "User-Agent: curl/8.4.0\r\n"
+	r += "Accept: */*\r\n\r\n"
+	r += "GET /qwe/rty HTTP/1.1\r\n"
+	r += "Host: localhost:4221\r\n"
+	r += "User-Agent: curl/8.4.0\r\n"
+	r += "Accept: */*\r\n\r\n"
 
 	reader := bufio.NewReader(strings.NewReader(r))
+	request := Request{}
 
-	p, err := s.readPacket(reader)
+	err := request.Read(reader)
 	assert.NoError(t, err)
-	assert.Equal(t, "GET", p.method)
-	assert.Equal(t, "/qwe/rty", p.request)
-	assert.Equal(t, "HTTP/1.1", p.version)
-	assert.Equal(t, "localhost:4221", p.headers["Host"])
-	assert.Equal(t, "curl/8.4.0", p.headers["User-Agent"])
-	assert.Equal(t, "*/*", p.headers["Accept"])
-	assert.Equal(t, []byte{}, p.body)
+
+	err = request.Read(reader)
+	assert.NoError(t, err)
+
+}
+
+func Test_404Response(t *testing.T) {
+
+	r := "GET /qwe/rty HTTP/1.1\r\n"
+	r += "Host: localhost:4221\r\n"
+	r += "User-Agent: curl/8.4.0\r\n"
+	r += "Accept: */*\r\n\r\n"
+
+	reader := bufio.NewReader(strings.NewReader(r))
+	request := Request{}
+	err := request.Read(reader)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "GET", request.method)
+	assert.Equal(t, "/qwe/rty", request.target)
+	assert.Equal(t, "HTTP/1.1", request.version)
+	assert.Equal(t, "localhost:4221", request.headers["Host"])
+	assert.Equal(t, "curl/8.4.0", request.headers["User-Agent"])
+	assert.Equal(t, "*/*", request.headers["Accept"])
+	assert.Equal(t, []byte{}, request.body)
+
+	s := NewServer(0)
+
+	response := s.respond(request)
+	assert.Equal(t, 404, response.code)
+	assert.Equal(t, "Not Found", response.reason)
 
 }
